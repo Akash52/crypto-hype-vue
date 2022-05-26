6d9bb86191f776ca4a4d15269f0e92fd941dbe45
Akash52 <ac8572611@gmail.com>
2022-05-26 11:35:30 +0530

    remove eslint warning

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,24 +2,20 @@
   <nav class="navbar navbar-dark bg-dark navbar-expand-sm">
     <div class="container">
       <a class="navbar-brand" href="#"> Crypto Hype Vue </a>
-      <div class="collapse navbar-collapse id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">
-                        Trending Listings
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link"  href="#">
-                        Exchnages
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                       Portfolio
-                    </a>
-                </li>
-          </ul>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              Trending Listings
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> Exchnages </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> Portfolio </a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
