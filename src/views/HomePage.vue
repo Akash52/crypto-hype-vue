4ad115c779ac8015c2cd7e2be1b43878c808d6fc
Akash52 <ac8572611@gmail.com>
2022-05-26 15:09:51 +0530

    Home Component Cards Section

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -22,6 +22,56 @@
         <img src="../assets/img/landing-right-bg.png" class="img-fluid" />
       </div>
     </div>
+    <div class="row mt-5 mb-5">
+      <div class="col-4">
+        <div class="card border-0 text-center">
+          <div class="card-body">
+            <img
+              src="../assets/img/trending.png"
+              class="img-fluid"
+              alt="bitcoin"
+            />
+            <h3 class="card-title mt-3">Bitcoin</h3>
+            <p class="card-text">Get the latest price and news for Bitcoin.</p>
+            <a href="#" class="btn btn-dark"> Trending Listings </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="card border-0 text-center">
+          <div class="card-body">
+            <img
+              src="../assets/img/exchanges.png"
+              class="img-fluid"
+              alt="bitcoin"
+            />
+            <h3 class="card-title mt-3">Bitcoin Exchanges</h3>
+            <p class="card-text">
+              Get the latest price and news for Bitcoin Exchanges.
+            </p>
+            <a href="#" class="btn btn-dark"> Exchanges </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="card border-0 text-center">
+          <div class="card-body">
+            <img
+              src="../assets/img/portfolio.png"
+              class="img-fluid"
+              alt="bitcoin"
+            />
+            <h3 class="card-title mt-3">Bitcoin Portfolio</h3>
+            <p class="card-text">
+              Get the latest price and news for your portfolio.
+            </p>
+            <a href="#" class="btn btn-dark"> Portfolio </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
